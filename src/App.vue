27f4ed3bb05f5f5287c73c94b5005a2fcd1cb73b
Pablo <>
2025-11-28 21:12:09 +0100

    feat(qubica): install dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const toggleDarkMode=()=> {
+    const root = document.documentElement
+
+    if (root.classList.contains('dark')) {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <div class="bg-red-500 dark:bg-green-500">
+      Header
+    </div>
+    <button @click="toggleDarkMode">Toggle dark mode</button>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+   <h1>Home</h1>
+  </main>
 </template>
 
 <style scoped></style>
