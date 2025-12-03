a532814139a2ea1adca8e9670f21e1204eb87468
Pablo <>
2025-12-03 15:07:40 +0100

    feat(bonus-points): implement transition animation views

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts"></script>
 <template>
   <main class="flex-1 w-full bg-gray-100 dark:bg-gray-900 p-5 md:p-10 2xl:p-20 overflow-auto">
-    <slot />
+    <Transition name="fade">
+      <slot />
+    </Transition>
   </main>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
